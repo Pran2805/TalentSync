c042b587c0003ba72a47bdfa93f5455dba3e69c7
Pran2805 <pranavshinde.as@gmail.com>
2025-10-31 21:29:01 +0530

    feat: Optimized Code backend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -5,16 +5,16 @@ import { ENV } from "./util/env";
 import express, { Request, Response } from 'express'
 
 app.use("/api/v1", router)
-// app.use("/health", (req:Request, res: Response) =>{
-//     res.send("HEllo World")
-// })
+app.use("/health", (_req:Request, res: Response) =>{
+    res.send("HEllo World")
+})
 const port = ENV.port || 5000
 
 const projectRoot = process.cwd();
 
 if (ENV.node_env === "production") {
     app.use(express.static(path.join(projectRoot, "../frontend/dist")))
-    app.get("/{*any}", (req: Request, res: Response) => {
+    app.get("/{*any}", (_req: Request, res: Response) => {
         res.sendFile(path.join(projectRoot, "../frontend/dist/index.html"))
     })
 }
