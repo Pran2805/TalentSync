e5413b9929d651c434bb95e34291a5757896d116
Pran2805 <pranavshinde.as@gmail.com>
2025-10-31 20:58:50 +0530

    feat: Basic Deployment Feature

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,12 +1,24 @@
+import path from "path";
+import router from "./route/index";
 import app from "./app";
-import router from "./route";
 import { ENV } from "./util/env";
-
+import express, { Request, Response } from 'express'
 
 app.use("/api/v1", router)
-
+// app.use("/health", (req:Request, res: Response) =>{
+//     res.send("HEllo World")
+// })
 const port = ENV.port || 5000
 
-app.listen(port, ()=>{
+const projectRoot = process.cwd();
+
+if (ENV.node_env === "production") {
+    app.use(express.static(path.join(projectRoot, "../frontend/dist")))
+    app.get("/{*any}", (req: Request, res: Response) => {
+        res.sendFile(path.join(projectRoot, "../frontend/dist/index.html"))
+    })
+}
+
+app.listen(port, () => {
     console.log('Server is running on Port', port)
 })
